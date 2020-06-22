932f638f32cca79379b88cb654a7190dbdc9a79b
mbruty <57625243+mbruty@users.noreply.github.com>
2020-06-22 11:03:44 +0100

    React website

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Shortener from './Shortener';
+
+function App() {
+  return (
+    <div>
+      <div class="title">Best</div>
+      <div class="title-boxed">Song</div>
+      <Shortener/>
+    </div>
+  );
+}
+
+export default App;
